dcd47dd94805943610d02fa85b0abdfdf25d4999
PC <PC@DESKTOP-B344JNV>
2024-10-30 11:42:03 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int B(int n) {
+	if (n == 1) return 2;
+    return 2 * B(n - 1);
+}
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	cout << B(6);
+
+	return 0;
+}
